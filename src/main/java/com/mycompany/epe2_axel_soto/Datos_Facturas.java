68a3ee60e6325f4d307182e0e07d4f8433c3a8c7
Axel Soto <joxel1234@gmail.com>
2023-10-18 19:52:09 -0300

    clases iniciadas

---
+++ src/main/java/com/mycompany/epe2_axel_soto/Datos_Facturas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.epe2_axel_soto;
+
+/**
+ *
+ * @author LAURA MORALES
+ */
+public class Datos_Facturas {
+    
+}
---
+++ src/main/java/com/mycompany/epe2_axel_soto/EPE2_Axel_Soto.java
@@ -0,0 +1,8 @@
+package com.mycompany.epe2_axel_soto;
+
+public class EPE2_Axel_Soto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
